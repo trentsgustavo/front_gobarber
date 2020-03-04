6ed660f2af4b59ed1998f2f10b72be397e85ad8e
Gustavo Trentini <gustavo.trentini@universo.univates.br>
2020-03-03 21:21:46 -0300

    Configuraçoes iniciais projeto

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     SharedArrayBuffer: "readonly",
     __DEV__: true
   },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
@@ -17,21 +18,31 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module"
   },
-  plugins: ["react", "jsx-a11y", "import", "react-hooks", "prettier"],
+  plugins: [
+    "react",
+    "jsx-a11y",
+    "import",
+    "react-hooks",
+    "prettier"
+  ],
   rules: {
-    "prettier/prettier": "error",
-    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
-    "import/prefer-default-export": "off",
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {extensions: ['.jsx', '.js']}
+    ],
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'no-console': ["error" , {allow: ["tron"]}],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
     "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     "react/jsx-one-expression-per-line": "off",
     "global-require": "off",
     "react-native/no-raw-text": "off",
-    "no-param-reassign": "off",
     "no-underscore-dangle": "off",
     camelcase: "off",
-    "no-console": ["error", { allow: ["tron"] }],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    "react/jsx-props-no-spreading": 'off'
   },
   settings: {
     "import/resolver": {
